af1249872b5d4a91c4b667228ff84d39c3780981
6abotage <61562467+6abotage@users.noreply.github.com>
2022-07-01 00:29:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="main">
+      <div className="circle one"></div>
+      <div className="circle two"></div>
+      <div className="circle three"></div>
+      <div className="circle four"></div>
+      <div className="circle five"></div>
+      <div className="circle six"></div>
+    </div>
+  );
+}
